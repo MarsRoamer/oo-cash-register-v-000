945cbbe931fefccd7509fb4080f6e89ec19c5a20
Jonathan Tiede <skysoldiermars@gmail.com>
2018-03-07 18:16:04 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 require 'pry'
 class CashRegister
 
-attr_accessor :total, :discount, :quantity, :title
+attr_accessor :total, :discount, :quantity, :title, :last_item, :last_item_price
 
 
   def initialize(employee_discount = 0)
@@ -13,6 +13,8 @@ attr_accessor :total, :discount, :quantity, :title
   def add_item(title, price, quantity = 1)
     @total += (price * quantity)
     @quantity = quantity
+    @last_item = title
+    @last_item_price = price
     if quantity > 1
       i = 0
       while i < quantity
@@ -42,6 +44,7 @@ attr_accessor :total, :discount, :quantity, :title
   end
 
   def void_last_transaction
+    @total = @total - @last_item_price
 
   end
 
